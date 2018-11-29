6a5ee4dafdbdadcdeb54ea6c47655f8a56154096
Arlinda Ujkani <you@example.com>
2018-11-29 23:15:27 +0100

    Erste Schritte: Bibliotheken und Datei laden

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -1,14 +1,12 @@
 # Analyse Script
-print("Hier werden Pakete eingebunden")
 
-# Data Cleaning ----
-print("Hier wird der Datensatz aufbereitet. Thema am 09.11.2018")
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("psych")
+install.packages("esquisse")
+install.packages("ggthemes")
+install.packages("ggplot2")
 
-# Skalenberechnung ----
-print("Hier werden später Skalen berechnet. Thema am 09.11.2018")
-
-# Analyse ----
-print("Hier werden später statistische Analysen durchgeführt. Thema ab dem 16.11.2018")
-
-# Graphik erstellung ----
-print("Hier werden später Grafiken erstellt. Thema ab dem 16.11.2018")
+install.packages("devtools")
+library(devtools)
+devtools::install_github("HCIC/r-tools")
