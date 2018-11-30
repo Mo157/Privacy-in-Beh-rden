26c41ff57c54b88a4911560eb45c40217f504a05
Arlinda Ujkani <you@example.com>
2018-11-30 01:28:06 +0100

    Unsere Daten sind nun gecleanded.

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -10,3 +10,10 @@ install.packages("ggplot2")
 install.packages("devtools")
 library(devtools)
 devtools::install_github("HCIC/r-tools")
+
+source("surveymonkey.R")
+
+
+#### Datei laden ----
+filename <- "data/Smart Identification.csv"
+raw <- load_surveymonkey_csv(filename)
